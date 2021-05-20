c2b75eeae1c513848c078dcd52783e90c15e1e94
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2021-05-20 23:36:04 +0500

    Added UI create task and moal function

---
+++ client/src/components/taskScreens/AddTaskModal.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect } from 'react';
+import { ModalWrapper } from './Styles';
+
+const AddTaskModal = ({ closeModal }) => {
+  let myRef;
+  useEffect(() => {
+    document.addEventListener('click', closeTodoModal);
+    return () => {
+      document.removeEventListener('click', closeTodoModal);
+    }
+  }, []);
+
+  const closeTodoModal = (e) => {
+    if (myRef && !myRef.contains(e.target)) {
+      closeModal();
+    }
+  }
+
+  return (
+    <ModalWrapper>
+      <form ref={(node) => (myRef = node)}>
+        <h4 className="text-center">Create Todo</h4>
+        <div className="form-group">
+          <input
+            type="text"
+            name="todo"
+            className="form-control"
+            placeholder="Enter new Todo"
+          />
+          <br/>
+           <input
+            type="text"
+            name="priority"
+            className="form-control"
+            placeholder="Enter Task Priority"
+          />      
+        </div>
+        <button
+          className="btn btn-primary float-right"
+        >
+          Save
+        </button>
+        <button
+          className="btn btn-danger float-right mr-1"
+          onClick={closeTodoModal}
+        >
+          Cancel
+        </button>
+      </form>
+    </ModalWrapper>
+  );
+};
+
+export default AddTaskModal;
--- client/src/components/taskScreens/ListTodo.jsx
+++ client/src/components/taskScreens/ListTodo.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Design from '../designs/Design';
+import { TodoList } from './Styles';
+import CreateTodoModal from './AddTaskModal';
 
 
-const ListTodos = () => {
+const ListTodo = () => {
+  const [showModal, setshowModal] = useState(false);
+
+  const handleShowModal = () => {
+    setshowModal(true);
+  }
+
+  const handleCloseModal = () => {
+    setshowModal(false);
+  }
+
   return (
     <Design>
-
+      <TodoList>
+        {showModal && <CreateTodoModal closeModal={handleCloseModal} />}
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 header text-center mt-3">
+              <h1>My Todos</h1>
+            </div>
+             <div className="col-md-12">
+              <button className="btn btn-primary float-right" onClick={handleShowModal}>New</button>
+            </div>
+          </div>
+          <div className="row">
+           
+          </div>
+        </div>
+      </TodoList>
     </Design>
   )
 }
-
-export default ListTodos;
+export default ListTodo;
